94977b03945f7be55f819e86cde53bcb9c17438b
Fisnik Shabani <fisnik.shabani@gmail.com>
2022-08-21 01:59:49 +0200

    added custom attributes to JWT

--- src/main/java/com/example/auth/JDBCAuthenticationProvider.java
+++ src/main/java/com/example/auth/JDBCAuthenticationProvider.java
@@ -12,8 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.*;
 
 @Singleton
 public class JDBCAuthenticationProvider implements AuthenticationProvider {
@@ -39,7 +38,17 @@ public class JDBCAuthenticationProvider implements AuthenticationProvider {
                 final String secret = (String) authenticationRequest.getSecret();
                 if (maybeUser.get().getPassword().equals(secret)) {
                     LOG.debug("User logged in.");
-                    emitter.onNext(new UserDetails(identity, new ArrayList<>()));
+
+                    final HashMap<String, Object> attributes = new HashMap<>();
+                    attributes.put("hair_color","brown");
+                    attributes.put("language", "sq");
+
+                    final UserDetails userDetails = new UserDetails(
+                            identity,
+                            Collections.singletonList("ROLE_USER"),
+                            attributes
+                    );
+                    emitter.onNext(userDetails);
                     emitter.onComplete();
                     return;
                 } else {
